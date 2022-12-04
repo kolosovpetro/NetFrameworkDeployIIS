a744a54e9e1fad1b81ad52ab6eb0c1603a7e353c
kolosovpetro <kolosovp94@gmail.com>
2022-12-04 15:36:16 +0100

    initial commit

---
+++ NetFrameworkDeployIIS/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NetFrameworkDeployIIS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Console.WriteLine();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            ViewBag.Message = $"Your application description page. Version: {assemblyVersion}";
+            ViewBag.ConnectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+## NET Framework IIS Deploy
+
+[![Build Web App](https://github.com/kolosovpetro/NetFrameworkDeployIIS/actions/workflows/build_and_publish.yml/badge.svg)](https://github.com/kolosovpetro/NetFrameworkDeployIIS/actions/workflows/build_and_publish.yml)
+
+### Enable IIS on Windows
+
+- Enable IIS using Control Panel
+  ![01_enable_iis_windows](./img/01_iis_activate.png)
+- Verify how IIS works on localhost
+  ![02_verify_iis](./img/02_iis_check_localhost.PNG)
+
+### Deployment process
+
+- Create a new IIS website
+  ![03_create_new_website](./img/03_iis_create_site.png)
+- Publish project to the website physical path
+- Validate the website
+  ![04_validate_website](./img/04_published_site_check.PNG)
+
+### Video how to deploy
+
+- https://www.youtube.com/watch?v=PPaqVyBkwMk
+
+### How to enable IIS on Windows server
+
+- https://thesecmaster.com/step-by-step-procedure-to-configure-iis-on-the-windows-server/
